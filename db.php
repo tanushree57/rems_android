af2fea10fb7506fd1822ee3a52212e24c9bba4ba
glenn_server <glennferns93@yahoo.com>
2016-04-09 01:55:03 +0530

    login

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-$conn=mysqli_connect("localhost", "root", "","rems_db");
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-//echo "Connected successfully";
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'rems_db');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
