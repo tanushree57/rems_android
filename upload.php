33201ff92c60faae33b1693037a8024096b54b4e
Glenn Fernandes <glennferns93@yahoo.com>
2016-04-10 16:15:27 +0530

    upload

---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Glenn
+ * Date: 10-04-2016
+ * Time: 04:13 PM
+ */
+move_uploaded_file($_FILES["file"]["tmp_name"], '/rems_android/images/file');
+
+
+
+
+
+?>
